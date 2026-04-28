7e3f956431bc76f71ace81079821b2ee332c34cd
Viswanatha Reddy Nagireddy <viswanathareddynagireddy@gmail.com>
2026-04-28 06:57:52 +0530

    [feature]: WKND-01 added multifield

---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/TopicItems.java
@@ -0,0 +1,17 @@
+package com.adobe.aem.guides.wknd.core.models;
+
+import lombok.Getter;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+@Model(adaptables = Resource.class,
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
+)
+public class TopicItems {
+
+    @Getter
+    @ValueMapValue
+    private String topic;
+}
